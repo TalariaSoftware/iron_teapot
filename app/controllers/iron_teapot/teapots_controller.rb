91090fb4f970633b3f2c0919ce425727305fac9c
Damien Burke <damien@damienburke.com>
2021-08-13 16:05:52 -0700

    Ignore authenticity token action if it’s not defined

--- app/controllers/iron_teapot/teapots_controller.rb
+++ app/controllers/iron_teapot/teapots_controller.rb
@@ -2,7 +2,7 @@ require_dependency 'iron_teapot/application_controller'
 
 module IronTeapot
   class TeapotsController < ApplicationController
-    skip_before_action :verify_authenticity_token
+    skip_before_action :verify_authenticity_token, raise: false
 
     def coffee
       render plain: '', status: 418
